233e0681ebf98acdb93009b3ecf9bb62de411f29
GUILHERME VIANA DE SOUZA <25132756@CFP127ALUNO.LOCAL>
2025-10-20 11:26:23 -0300

    Quase finalizado o formulário inicial com todas as formatações. Faltam a busca, requisição da api e outros layouts para finalização do projeto.

--- app.js
+++ app.js
@@ -8,33 +8,57 @@ const escopo = document.getElementById('escopo')
     const whatsappFormulario = document.createElement('div')
     whatsappFormulario.classList.add('formulario')
 
+    const whatsappLabel = document.createElement('label');
+    whatsappLabel.textContent = 'Número de WhatsApp:';
+    whatsappLabel.htmlFor = 'whatsappInput';
+
     const whatsappInput = document.createElement('input')
     whatsappInput.type = 'tel'
     whatsappInput.placeholder = 'Digite seu número'
     whatsappInput.maxLength = '11'
     whatsappInput.pattern = '\d{11}'
-
     whatsappInput.classList.add('informe-numero')
 
     const botaoAcessar = document.createElement('button')
+    botaoAcessar.textContent = 'Acessar'
     botaoAcessar.classList.add('acessar-whatsapp')
 
-    
+    botaoAcessar.addEventListener('click', function() {
+        const valorDigitado = whatsappInput.value
+
+        confirmandoWhatsapp(valorDigitado)
+    })
+
+    whatsappFormulario.appendChild(whatsappLabel)
     whatsappFormulario.appendChild(whatsappInput)
     whatsappFormulario.appendChild(botaoAcessar)
-
     entradaInformacoes.appendChild(whatsappFormulario)
 
     return entradaInformacoes
-                        } 
+    } 
 
 
 async function confirmandoWhatsapp(numero){
     try {
-        const response = await fetch(`http://localhost:8080/v1/whatsapp/informationforcontact/:${numero}.json`)
+        const url = (`https://whatsapp-back-end.onrender.com/v1/whatsapp/informationforcontact/${numero}.json`)
+        const resposta = await fetch(url)
+
+        if(!resposta.ok){
+            throw new error(`Erro da API: ${resposta.status} ${resposta.statusText}`);
+        }
+
+        const informacoes = await resposta.json()
+        console.log("Sucesso, dados recebidos:", informacoes);
     } catch (error) {
         
     }
 }
 
+async function contatosWhatsapp(params) {
+    
+}
+
+async function 
+
 mostrarFormulario(entradaInformacoes)
+confirmandoWhatsapp()
