b360925f954ebb790b19f000e6f7eee60a8dd0ed
GUILHERME VIANA DE SOUZA <25132756@CFP127ALUNO.LOCAL>
2025-10-27 08:32:06 -0300

    Lógica de fetch por trás do formulário finalizada. Iniciando processo de limpeza da tela para acesso a área de contatos.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 'use strict'
 
+// Declaro o cabecario, o corpo e o escopo da tela
+
 const cabecario = document.getElementById('cabecario')
 const entradaInformacoes = document.querySelector('.entrada-informacoes')
 const escopo = document.getElementById('escopo')
+
+// Aqui eu monto o formulario que será apresentado na tela.
 
     function mostrarFormulario(entradaInformacoes){
     const whatsappFormulario = document.createElement('div')
@@ -13,9 +17,9 @@ const escopo = document.getElementById('escopo')
     whatsappLabel.htmlFor = 'whatsappInput';
 
     const whatsappInput = document.createElement('input')
-    whatsappInput.type = 'tel'
+    whatsappInput.type = 'tel' //defino o input como para telefone
     whatsappInput.placeholder = 'Digite seu número'
-    whatsappInput.maxLength = '11'
+    whatsappInput.maxLength = '11' //coloco um limitador máximo
     whatsappInput.pattern = '\d{11}'
     whatsappInput.classList.add('informe-numero')
 
@@ -40,7 +44,7 @@ const escopo = document.getElementById('escopo')
 
 async function confirmandoWhatsapp(numero){
     try {
-        const url = (`https://whatsapp-back-end.onrender.com/v1/whatsapp/informationforcontact/${numero}.json`)
+        const url = (`https://whatsapp-back-end.onrender.com/v1/whatsapp/informationforcontact/${numero}`)
         const resposta = await fetch(url)
 
         if(!resposta.ok){
@@ -48,17 +52,24 @@ async function confirmandoWhatsapp(numero){
         }
 
         const informacoes = await resposta.json()
-        console.log("Sucesso, dados recebidos:", informacoes);
+        document.body.innerHTML = ''
+        contatosWhatsapp()
     } catch (error) {
-        
+        return mostrarFormulario
     }
 }
 
-async function contatosWhatsapp(params) {
+async function contatosWhatsapp() {
+    document.body.appendChild(cabecario)
+    document.body.appendChild(entradaInformacoes)
+    document.body.appendChild(escopo)
+
+    entradaInformacoes.innerHTML = ''
+
     
 }
 
-async function 
+async function limpandoTela (){}
 
 mostrarFormulario(entradaInformacoes)
 confirmandoWhatsapp()
